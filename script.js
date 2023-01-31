1892600988c8484a454f7d8a7a7706c04b56a69a
yugsharma4 <65899617+yugsharma4@users.noreply.github.com>
2023-01-31 17:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+const h3 = document.getElementById("url");
+const name = document.getElementById("name");
+const year = document.getElementById("year");
+const button = document.getElementById("button");
+
+button.addEventListener('click', () => {
+   const inputName = name.value;
+   const inputYear = year.value;
+   if(inputName && !inputYear) h3.textContent += "?name=" + inputName;
+   else if(!inputName && inputYear) h3.textContent += "?year=" + inputYear;
+   else h3.textContent += "?name=" + inputName + "&year="+inputYear;
+});
+
